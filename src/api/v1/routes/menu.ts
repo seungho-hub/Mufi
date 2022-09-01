e9feb4ad335d712265a6e622344f00123339456c
seungho-hub <kmc54320@gmail.com>
2022-09-02 07:08:48 +0900

    ADD : menu router,controller

--- src/api/v1/routes/menu.ts
+++ src/api/v1/routes/menu.ts
@@ -1,8 +1,15 @@
 import { Router } from "express"
-import * as menuController from "../controller/menu"
 export const menu = Router()
+import { getMenu, createMenu } from "../controller/menu"
 
-menu.get("/", menuController.renderMenu)
+//menu router will be bounded at "/api/v1/menu"
+// GET "/api/v1/menu"
+menu.get("/")
+
+// POST "/api/v1/menu"
+menu.post("/", createMenu)
+
+
 
 
 
