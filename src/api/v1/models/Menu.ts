3714b228f51326e16c426572cde44cee162b14f8
seungho-hub <kmc54320@gmail.com>
2022-09-04 13:10:15 +0900

    UPDATE : menu id autoincrement int to varchar

--- src/api/v1/models/Menu.ts
+++ src/api/v1/models/Menu.ts
@@ -3,29 +3,34 @@ import { DataTypes, NOW, DataType } from 'sequelize';
 import Store from "./Store"
 
 const Menu = sequelize.define("Menu", {
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
     label: {
         type: DataTypes.STRING,
-        allowNull : false,
+        allowNull: false,
     },
     price: {
         type: DataTypes.INTEGER,
-        allowNull : false,
+        allowNull: false,
     },
     description: {
         type: DataTypes.TEXT,
     },
-    image : {
-        type : DataTypes.STRING,
-        allowNull : false,
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    store_id : {
-        type : DataTypes.STRING,
-        allowNull : false
+    store_id: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 })
 
 Menu.belongsTo(Store, {
-    foreignKey : "store_id"
+    foreignKey: "store_id"
 })
 
 export default Menu
