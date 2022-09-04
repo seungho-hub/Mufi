b2d48eeb0a1872f00bf834b68cfe9e1d93cf1bb8
seungho-hub <kmc54320@gmail.com>
2022-09-04 13:14:22 +0900

    ADD : create, delete menu

--- src/api/v1/routes/menu.ts
+++ src/api/v1/routes/menu.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import * as menuController from "../controller/menu"
-import {getMenu, createMenu} from "../controller/menu"
+import { getMenu, createMenu, deleteMenu } from "../controller/menu"
 
 export const menu = Router()
 
 menu.get("/", getMenu)
 
 menu.post("/", createMenu)
+
+menu.delete("/", deleteMenu)
 
 
 
