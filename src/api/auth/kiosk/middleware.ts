89350bbfc2b55a9099718e42aed9f8a074a79873
seungho-hub <kmc54320@gmail.com>
2022-09-25 03:05:40 +0900

    ADD : kiosk authorization middleware

---
+++ src/api/auth/kiosk/middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express"
+
+export const checkGotStoreAuthorization = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.session)
+    if (req.session && req.session.kiosk && req.session.kiosk.store_id) {
+        return next()
+    }
+
+    res.redirect("/auth/kiosk/store")
+}
+
+export const checkGotUserAuthorization = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.session)
+    if (req.session && req.session.kiosk && req.session.kiosk.user_id) {
+        return next()
+    }
+
+    res.redirect("/auth/kiosk/user")
+}   
