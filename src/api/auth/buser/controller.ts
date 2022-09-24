b531319addf217cd8a5932c2a1cfbd037f3a9807
seungho-hub <kmc54320@gmail.com>
2022-09-25 03:32:09 +0900

    ADD : email overlab exception

--- src/api/auth/buser/controller.ts
+++ src/api/auth/buser/controller.ts
@@ -83,6 +83,16 @@ export const signup = async (req: Request, res: Response) => {
         return
     }
 
+    const emailOberlabUser = await Buser.findOne({ where: { email } })
+
+    //user already exist with email
+    if (emailOberlabUser) {
+        res.status(400).render("buser/auth/signup", {
+            error: "해당 이메일로 등록된 사용자가 존재합니다"
+        })
+        return
+    }
+
     //check password mismatch
     if (password1 != password2) {
         res.status(400).render("buser/auth/signup", {
