7a6225a3d519e51a6b7dfc25194ac2379a20e9d0
seungho-hub <kmc54320@gmail.com>
2022-09-26 00:55:00 +0900

    ADD : render kiosk home

--- src/kiosk/index.ts
+++ src/kiosk/index.ts
@@ -1,6 +1,32 @@
 import { Request, Response } from "express"
+import Store from "../api/models/Store"
+import User from "../api/models/User"
+import Menu from "../api/models/Menu"
 
-export const renderHome = (req: Request, res: Response) => {
+export const renderHome = async (req: Request, res: Response) => {
+    const store = await Store.findOne({
+        where: {
+            id: req.session.kiosk.store_id
+        }
+    })
 
-    res.render("kiosk/home")
+    const user = await User.findOne({
+        where: {
+            id: req.session.kiosk.user_id,
+        }
+    })
+
+    const menu = await Menu.findAll({
+        where: {
+            store_id: req.session.kiosk.store_id
+        }
+    })
+
+    console.log(req.session.kiosk.store_id)
+
+    res.render("kiosk/home", {
+        store,
+        user,
+        menu
+    })
 }
