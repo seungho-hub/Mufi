8779bca37572dd83088bacc890488ef7be38c84c
TheLifeOfSeo <dsa3182@gmail.com>
2022-11-16 15:52:43 +0900

    fix menu page

---
+++ views/statics/js/menu/menuIndex.js
@@ -0,0 +1,20 @@
+export const divMenu =
+`<div class="uk-padding-small">
+    <div class="uk-grid-medium uk-flex-middle" uk-grid>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div class="uk-width-expand">
+            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">Author</a></h4>
+            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+                <li id="menu-price">12 days ago</li>
+                <li id="menu-date">Reply</li>
+            </ul>
+        </div>
+        <div class="uk-width-auto">
+            <p id="menu-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="uk-width-auto" id="div-delete">
+        </div>
+    </div>
+</div>`;
