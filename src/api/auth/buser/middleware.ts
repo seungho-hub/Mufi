c9b6efa7c3c0de08e7fbf7f7bfbcc5afc0be977b
seungho-hub <kmc54320@gmail.com>
2022-12-23 00:29:45 +0900

    FIX : solve uncatched exception

--- src/api/auth/buser/middleware.ts
+++ src/api/auth/buser/middleware.ts
@@ -1,17 +1,30 @@
 import { Request, Response, NextFunction } from "express"
 import { unless } from "express-unless"
-
+import BUser from "../../models/BUser"
 export function bUserAuthenticated(req: Request, res: Response, next: NextFunction) {
-    //authenticated
-    //condition : client got session and user data
+    //조건1. client가 session을 소유한 상태
     if (req.session && req.session.buser) {
-        return next()
-    }
-    //not authenticated redirect to signin page
-    else {
+        //조건2. client가 소유한 session이 db에 존재하는 buser에 대한 session이어야 함.
+        BUser.findOne({
+            where: {
+                id: req.session.buser.id
+            }
+        })
+            .then((buser) => {
+                if (buser) {
+                    next()
+                }
+
+            })
+            .catch(() => {
+
+            })
+    } else {
         res.redirect("/auth/buser/signin")
         return
     }
+
+    return
 }
 
 //for except authentication router
