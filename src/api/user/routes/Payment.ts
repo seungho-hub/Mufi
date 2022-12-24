3d21f395f8685071a90915587ccdc46aa65fbc75
seungho-hub <kmc54320@gmail.com>
2022-12-24 20:15:10 +0900

    UPDATE : use user authentication middleware to evenry user's payment api

--- src/api/user/routes/Payment.ts
+++ src/api/user/routes/Payment.ts
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import { getPayments } from "../controller/Payment"
+import { userAuthenticated } from "../../auth/user/middleware"
 export const PaymentRouter = Router()
 
 // "/api/user/payment"
+PaymentRouter.use(userAuthenticated)
 PaymentRouter.get("/", getPayments)
--- src/api/user/routes/uin.ts
+++ src/api/user/routes/uin.ts
@@ -1,5 +1,8 @@
 import { Router } from "express"
 import { generateUin } from "../controller/uin"
 export const uinRouter = Router()
+import { userAuthenticated } from "../../auth/user/middleware"
+
+uinRouter.use(userAuthenticated)
 
 uinRouter.get("/", generateUin)
