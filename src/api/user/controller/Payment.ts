ed402364aeb4c8cc2a56a4b5954d365c89115591
seungho-hub <kmc54320@gmail.com>
2022-12-24 20:14:10 +0900

    ADD : get user's payment api

--- src/api/user/controller/Payment.ts
+++ src/api/user/controller/Payment.ts
@@ -1,25 +1,45 @@
 import { Request, Response } from "express";
 import Payment from "../../models/Payment";
+import { Model } from "sequelize"
 
 export const getPayments = async (req: Request, res: Response) => {
-    const payments = await Payment.findAll({
-        where: {
-            user_id: req.session.user.id
-        },
-        attributes: [
-            "id",
-            "card_company",
-            "card_type",
-            "card_number",
-            "card_owner_type",
-            "createdAt",
-        ]
-    })
+    const targetPaymentId = req.query.targetPaymentId
+    let result: Model<any>
 
-    res.status(200).json({
-        code: 200,
-        data: payments
-    })
+
+    if (targetPaymentId) {
+        result = await Payment.findOne({
+            where: {
+                id: targetPaymentId,
+                user_id: req.session.user.id
+            }
+        })
+
+        res.json({
+            code: 200,
+            data: result
+        })
+    } else {
+        const result = await Payment.findAll({
+            where: {
+                user_id: req.session.user.id
+            },
+            attributes: [
+                "id",
+                "card_company",
+                "card_type",
+                "card_number",
+                "card_owner_type",
+                "createdAt",
+            ]
+        })
+
+        res.status(200).json({
+            code: 200,
+            data: result
+        })
+    }
+
 }
 
 export const deletePayment = async (req: Request, res: Response) => {
