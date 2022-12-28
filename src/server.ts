e8926accd4ac25ca5abe615c532bf009c19f1861
Seo <dsa3182@gmail.com>
2022-12-28 21:18:59 +0900

    change force false

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ function onListening() {
         typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
 
     //connect to database
-    sequelize.sync({ force: true })
+    sequelize.sync({ force: false })
         .then(() => {
             if (process.env.INIT) {
                 server.close()
