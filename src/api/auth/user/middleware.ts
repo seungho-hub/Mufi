8de74de119b766fa5ddb0878d02e22af84bfc14d
seungho-hub <kmc54320@gmail.com>
2023-01-12 22:20:27 +0900

    FIX : uncatched exception of buser authentication

--- src/api/auth/user/middleware.ts
+++ src/api/auth/user/middleware.ts
@@ -1,11 +1,31 @@
 import { Request, Response, NextFunction } from "express"
 import { unless } from "express-unless"
+import User from "../../models/User"
 
 export function userAuthenticated(req: Request, res: Response, next: NextFunction) {
     //authenticated
-    //condition : client got session and user data
+    //user정보에대한 session data를 가지고있음.
     if (req.session && req.session.user) {
-        return next()
+        User.findOne({
+            where: {
+                id: req.session.user.id
+            }
+        })
+            //session data의 user정보로 user를 찾을 수 있음.
+            .then((user) => {
+                if (user) {
+                    next()
+                } else {
+                    res.redirect("/auth/user/signin")
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    code: 500,
+                    message: "알 수 없는 에러가 발생했습니다."
+                })
+            })
+        return
     }
     //not authenticated redirect to signin page
     else {
